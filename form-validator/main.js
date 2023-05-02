64232c485775f7e300c6b1d53c3043a7f105bfa0
Ali <86948893+AleksHNZ@users.noreply.github.com>
2023-05-02 16:31:32 +0400

    Test

---
+++ form-validator/main.js
@@ -0,0 +1,6 @@
+const password = document.querySelector('#password');
+
+function validatePassword(password) {
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    
+}
