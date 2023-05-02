98c13514d3be80baf9a100260e4fba92a085e24c
Ali <86948893+AleksHNZ@users.noreply.github.com>
2023-05-02 17:07:40 +0400

    Changes in JS

--- form-validator/main.js
+++ form-validator/main.js
@@ -1,6 +1,13 @@
-const password = document.querySelector('#password');
+const passwordInput = document.querySelector('#password');
+const passwordValidation = document.querySelector('#password-validation');
 
-function validatePassword(password) {
-    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-    
-}
+passwordInput.addEventListener('submit', () => {
+  const password = passwordInput.value;
+  if (password.length < 8) {
+    passwordValidation.textContent = 'Password must be at least 8 characters long';
+  } else if (!/\d/.test(password) || !/[a-zA-Z]/.test(password)) {
+    passwordValidation.textContent = 'Password must contain both letters and numbers';
+  } else {
+    passwordValidation.textContent = 'Password is valid';
+  }
+});
